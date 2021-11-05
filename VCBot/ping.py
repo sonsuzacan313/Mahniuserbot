04a0259964cd9a94470290d558157484066bdde1
Yushkabotss <92757522+Yushkabotss@users.noreply.github.com>
2021-11-06 01:11:57 +0400

    Update ping.py

--- VCBot/ping.py
+++ VCBot/ping.py
@@ -46,7 +46,7 @@ async def restart(client, m: Message):
    # You probably don't need it but whatever
    quit()
 
-@Client.on_message(contact_filter & filters.command(['/'], prefixes=f"/"))
+@Client.on_message(contact_filter & filters.command(['start'], prefixes=f"/"))
 async def help(client, m: Message):
    HELP = f"**işləmə qaydası 🛠** \n\n__USER əmrləri__ (👑): \n`/play` \n`/yayim` \n`/canli` (Radio linki) \n`/yayim` (.m3u8 / canlı link) \n`/kanal [kanal linki] ; [n]` - kanaldan mahnı seçərək səsləndirər \n`/siyahi` / `/playlist` \n\n__SUDO COMMANDS__ (Can only be accessed by You and Your Contacts): \n`/ping` \n`/skip` \n`/pause` and `/resume` \n`/stop` / `/end` \n`/help` \n`!restart`"
    await m.reply(HELP)
